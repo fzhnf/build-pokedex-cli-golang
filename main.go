0972ecb7930f3b14d5cf647006cc67240d1261d6
Tima Gutmensch <tima_gutmensch@proton.me>
2026-08-15 23:48:25 +0800

    Cache: PokeAPI

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -29,8 +33,70 @@ exit: Exit the Pokedex
 	return nil
 }
 
+type locationArea struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type locationAreaResponse struct {
+	Count    int            `json:"count"`
+	Next     *string        `json:"next"`
+	Previous *string        `json:"previous"`
+	Results  []locationArea `json:"results"`
+}
+
+func commandMap(cfg *config) error {
+	var requestURL string
+	if cfg.locationAreaNextPage != nil {
+		requestURL = *cfg.locationAreaNextPage
+	} else {
+		requestURL = "https://pokeapi.co/api/v2/location-area"
+	}
+
+	res, err := http.Get(requestURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	locationAreas := locationAreaResponse{}
+	if err := json.Unmarshal(body, &locationAreas); err != nil {
+		log.Fatal(err)
+	}
+
+	cfg.locationAreaNextPage = locationAreas.Next
+	cfg.locationAreaPrevPage = locationAreas.Previous
+
+	for _, locationArea := range locationAreas.Results {
+		fmt.Printf("%s\n", locationArea.Name)
+	}
+
+	return nil
+}
+
+func commandMapBack(cfg *config) error {
+	if cfg.locationAreaPrevPage == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	requestURL := *cfg.locationAreaPrevPage
+	cfg.locationAreaNextPage = &requestURL
+	commandMap(cfg)
+
+	return nil
+}
+
 type config struct {
-	commands map[string]cliCommand
+	commands             map[string]cliCommand
+	locationAreaNextPage *string
+	locationAreaPrevPage *string
 }
 
 type cliCommand struct {
@@ -39,9 +105,10 @@ type cliCommand struct {
 	callback    func(*config) error
 }
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	cfg := config{
+func newConfig() *config {
+	return &config{
+		locationAreaNextPage: nil,
+		locationAreaPrevPage: nil,
 		commands: map[string]cliCommand{
 			"exit": {
 				name:        "exit",
@@ -53,23 +120,41 @@ func main() {
 				description: "Displays a help message",
 				callback:    commandHelp,
 			},
+			"map": {
+				name:        "map",
+				description: "Displays the names of location areas in the Pokemon world",
+				callback:    commandMap,
+			},
+			"mapb": {
+				name:        "mapb",
+				description: "Displays the previous names of location areas in the Pokemon world",
+				callback:    commandMapBack,
+			},
 		},
 	}
+}
+
+func startRepl(cfg *config) {
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex> ")
 		if scanner.Scan() {
 			text := scanner.Text()
 			textCleaned := cleanInput(text)
 			if len(textCleaned) == 0 || textCleaned[0] == "" {
-				cfg.commands["help"].callback(&cfg)
+				cfg.commands["help"].callback(cfg)
 				continue
 			}
 
 			if command, ok := cfg.commands[textCleaned[0]]; !ok {
-				cfg.commands["help"].callback(&cfg)
+				cfg.commands["help"].callback(cfg)
 			} else {
-				command.callback(&cfg)
+				command.callback(cfg)
 			}
 		}
 	}
+}
+
+func main() {
+	startRepl(newConfig())
 }
