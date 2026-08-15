b36113a1b350195fff9c00e92c9e9b2962eca668
Tima Gutmensch <tima_gutmensch@proton.me>
2026-08-15 13:40:15 +0800

    REPL: REPL

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
-	return strings.Fields(text)
+	return strings.Fields(strings.ToLower(text))
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex> ")
+		if scanner.Scan() {
+			text := scanner.Text()
+			textCleaned := cleanInput(text)
+			fmt.Printf("Your command was: %s\n", textCleaned[0])
+		}
+	}
 }
