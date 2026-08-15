361dfb6935526a4ebc4b1b6ed205abca44385094
Tima Gutmensch <tima_gutmensch@proton.me>
2026-08-15 19:20:51 +0800

    REPL: Help and Exit

--- main.go
+++ main.go
@@ -11,14 +11,62 @@ func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	multilineHelp := `
+Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+`
+	fmt.Println(multilineHelp)
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	cliCommands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 	for {
 		fmt.Print("Pokedex> ")
 		if scanner.Scan() {
 			text := scanner.Text()
 			textCleaned := cleanInput(text)
-			fmt.Printf("Your command was: %s\n", textCleaned[0])
+			if textCleaned[0] == "" {
+				cliCommands["help"].callback()
+				continue
+			}
+			// if _, ok := cliCommands[textCleaned[0]]; !ok {
+			// 	cliCommands["help"].callback()
+			// }
+			command, ok := cliCommands[textCleaned[0]]
+			if !ok {
+				cliCommands["help"].callback()
+				continue
+			}
+			command.callback()
 		}
 	}
 }
